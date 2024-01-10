250d20c6ad52dcb61979a84a00b55174a0c01e9f
pshistory <psh_a@naver.com>
2024-01-10 16:36:53 +0900

    comfirm Test

--- Login.java
+++ Login.java
@@ -1,6 +1,8 @@
 Public class Login(){
 	public static void Main(String[] args){
-		System.out.println("Hellow Spring");
+	System.out.println("Hellow Spring";
+			System.out.println("Hellow Spring";
+
 	}
 }
 
