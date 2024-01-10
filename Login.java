07bb1caec94d6c1b7c2be9c040cbd711f6eec060
dosalpark <154612223+dosalpark@users.noreply.github.com>
2024-01-10 16:48:17 +0900

    Revert "re"

--- Login.java
+++ Login.java
@@ -10,7 +10,7 @@ Public class Login(){
 		System.out.println("jhwan이 추가로 입력하였습니다.");
 		System.out.println("Confict");
 		System.out.println("jhwan conflit 해결 ");
-System.out.println("jhwan conflit □~U□결 ");
+
 	}
 }
 
