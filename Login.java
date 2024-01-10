e958fec3db44604a4b9571109a52e19200c5f493
jhwan <jhwan.kim95@gmail.com>
2024-01-10 15:24:02 +0900

    update : jhwan이login의 일부를 수정함

--- Login.java
+++ Login.java
@@ -1,6 +1,10 @@
 Public class Login(){
 	public static void Main(String[] args){
 		System.out.println("Hellow Spring");
+		System.out.println("jhwan이 추가로 입력하였습니다.");
+		System.out.println("jhwan이 추가로 입력하였습니다.");
+		System.out.println("jhwan이 추가로 입력하였습니다.");
+		System.out.println("jhwan이 추가로 입력하였습니다.");
 	}
 }
 
